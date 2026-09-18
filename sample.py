7bae5a809408af814cdd3f203a263a2c0331b2ec
unknown <aincejoyvisbal@gmail.com>
2026-09-18 16:37:57 +0800

    new version

--- sample.py
+++ sample.py
@@ -1,2 +1,8 @@
 print("Hello, GitHub!")
 print("This is the old version of our program.")
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+print("Hello,", name + "!")
+print("You are", age, "years old.")
